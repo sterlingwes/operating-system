f29147e990fcc99833dcc9a91a511877e1ae3f35
Wes Johnson <swj@wesquire.ca>
2020-05-03 18:40:46 -0400

    add cypress & window mgmt tests

---
+++ cypress/integration/window-mgmt.spec.js
@@ -0,0 +1,66 @@
+/* global cy, before, assert */
+
+describe('Window management', () => {
+  before(() => cy.visit('http://localhost:3000'))
+
+  describe('desktop icon', () => {
+    const targetAppLabel = 'MS-DOS Prompt'
+
+    describe('single click', () => {
+      it('should focus the icon', () => {
+        cy.contains(targetAppLabel).click().parent().should('be.focused')
+      })
+    })
+
+    describe('double click', () => {
+      it('should open the target app', () => {
+        cy.get('.title-bar').should('have.length', 0)
+        cy.contains(targetAppLabel).dblclick()
+        cy.get('.title-bar').should('have.length', 1)
+      })
+
+      it('should open multiple apps if repeated', () => {
+        cy.get('.title-bar').should('have.length', 1)
+        cy.contains(targetAppLabel).dblclick()
+        cy.get('.title-bar').should('have.length', 2)
+      })
+    })
+  })
+
+  describe('window events', () => {
+    describe('focusing a background window', () => {
+      let initialWindow
+
+      it('should bring it to the foreground', () => {
+        const backgroundWindow = cy
+          .get('.title-bar')
+          .first()
+          .should(($first) => {
+            initialWindow = $first[0]
+            assert.isOk(initialWindow)
+          })
+
+        backgroundWindow.first().click()
+
+        cy.get('.title-bar')
+          .last()
+          .should(($foregrounded) => {
+            assert.equal(initialWindow, $foregrounded[0])
+          })
+      })
+
+      describe('closing the other window', () => {
+        it('should leave the foregrounded window active', () => {
+          const activeWindows = cy.get('.title-bar')
+          activeWindows.should('have.length', 2)
+          activeWindows.first().find('[aria-label="Close"]').click()
+          cy.get('.title-bar')
+            .should('have.length', 1)
+            .should(($el) => {
+              assert.equal($el[0], initialWindow)
+            })
+        })
+      })
+    })
+  })
+})
