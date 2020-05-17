dd389de4f50258967957039143aa151687c82aa2
Wes Johnson <swj@wesquire.ca>
2020-05-17 00:04:38 -0400

    wip adding semantic release

---
+++ ci/semantic-release-monorepo.js
@@ -0,0 +1,85 @@
+const semanticReleaseNpm = require('@semantic-release/npm')
+const semanticReleaseGit = require('@semantic-release/git')
+const semanticReleaseGithub = require('@semantic-release/github')
+const semanticReleaseChanelog = require('@semantic-release/changelog')
+const {
+  analyzeCommits,
+  generateNotes,
+  tagFormat,
+} = require('semantic-release-monorepo')
+
+const noPublishNpmConfig = (pluginConfig) => ({
+  ...pluginConfig,
+  npmPublish: false,
+})
+
+const configure = (context) => ({
+  ...context,
+  options: {
+    ...(context.options || {}),
+    publish: [
+      {
+        path: '@semantic-release/npm',
+        npmPublish: false,
+      },
+    ],
+  },
+})
+
+const commitMessageConfig = (pluginConfig) => ({
+  ...pluginConfig,
+  assets: ['CHANGELOG.md', 'package.json'],
+  message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+})
+
+/**
+ * default semantic-release plugins:
+ *
+ * @semantic-release/commit-analyzer
+ * @semantic-release/release-notes-generator
+ * @semantic-release/npm
+ * @semantic-release/github
+ */
+
+module.exports = {
+  analyzeCommits,
+  generateNotes,
+  tagFormat,
+
+  verifyConditions: async (pluginConfig, context) => {
+    semanticReleaseGit.verifyConditions(
+      commitMessageConfig(pluginConfig),
+      context
+    )
+    await semanticReleaseChanelog.verifyConditions(pluginConfig, context)
+    await semanticReleaseNpm.verifyConditions(
+      noPublishNpmConfig(pluginConfig),
+      configure(context)
+    )
+    await semanticReleaseGithub.verifyConditions(pluginConfig, context)
+  },
+
+  prepare: async (pluginConfig, context) => {
+    await semanticReleaseChanelog.prepare(pluginConfig, context)
+    await semanticReleaseNpm.prepare(
+      noPublishNpmConfig(pluginConfig),
+      configure(context)
+    )
+    await semanticReleaseGit.prepare(commitMessageConfig(pluginConfig), context)
+  },
+
+  publish: async (pluginConfig, context) => {
+    await semanticReleaseNpm.publish(
+      noPublishNpmConfig(pluginConfig),
+      configure(context)
+    )
+    await semanticReleaseGithub.publish(pluginConfig, context)
+  },
+
+  addChannel: async (pluginConfig, context) => {
+    await semanticReleaseNpm.addChannel(
+      noPublishNpmConfig(pluginConfig),
+      configure(context)
+    )
+  },
+}
